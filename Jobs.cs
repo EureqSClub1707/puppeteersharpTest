82322737694d05ceb10a517345f6290ba3ff2742
EureqSClub1707 <36820511+EureqSClub1707@users.noreply.github.com>
2020-09-03 00:46:52 +0300

    Add files via upload

---
+++ Jobs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Algo
+    {
+        public string Action { get; set; }
+        public string XPath { get; set; }
+        public int Timeout { get; set; }
+    }
+
+    public class Work
+    {
+        public int id { get; set; }
+        public string URL { get; set; }
+        public string Referrer { get; set; }
+        public List<Algo> Algo { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Work> Works { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+using Newtonsoft.Json;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sJobs = System.IO.File.ReadAllText("Jobs/manifest.json");
+            var Jobs = JsonConvert.DeserializeObject<Root>(sJobs);
+
+            Browser(Jobs);
+        }
+        static async void Browser(Root root)
+        {
+            new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+
+            var browser = Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false
+            });
+
+            var bro = browser;
+            var page = bro.GetAwaiter().GetResult().NewPageAsync().GetAwaiter().GetResult();
+
+            NavigationOptions options = new NavigationOptions();
+            foreach (var work in root.Works)
+            {
+                options.Referer = work.Referrer;
+
+                var request = page.GoToAsync(work.URL, options).GetAwaiter().GetResult();
+
+                for (int i = 0; i < work.Algo.Count; i++)
+                {
+                    var waiter = page.WaitForXPathAsync(work.Algo[i].XPath);
+
+                    page.ScreenshotAsync(i + ".jpg");
+                    page.ClickAsync(work.Algo[i].XPath).GetAwaiter().GetResult();
+
+                    Console.WriteLine(work.Algo[i].XPath);
+                }
+            }
+        }
+    }
+}
